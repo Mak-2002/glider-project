427c9f40d2cd8c69f96fab59c63385c0915f6fae
Qusai Nasr <kossay.ar.en@gmail.com>
2023-08-09 05:50:14 +0300

    added sliders in UI

--- physics.js
+++ physics.js
@@ -1,15 +1,15 @@
 
 // Constant factors in the study
-const g = 9.8; // acceleration due to gravity
-const R = 287.058; // specific gas constant for dry air
+const g = 9.8; //? acceleration due to gravity
+const R = 287.058; //? specific gas constant for dry air
 const K4 = 0,
     K5 = 0,
-    a1 = 0; // experimental constants for lift coefficient
+    a1 = 0; //? experimental constants for lift coefficient
 
 // Variable factors in the study
-let m; // mass of the glider
+let m; //? mass of the glider
 
-// Geometric characteristics of the glider:
+//? Geometric characteristics of the glider:
 let wingspan;
 let wingarea;
 let fuselageLength;
@@ -17,17 +17,23 @@ let fuselageHeight;
 let tailHeight;
 let tailSpan;
 
-let Vf; // wind speed relative to the wing of the glider
+let Vf; //? wind speed relative to the wing of the glider
 
 let I = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
-]; // moment of inertia tensor
+]; //? moment of inertia tensor
 
-let T; // air temperature
-let Pr; // atmospheric pressure
-let CD; // drag coefficient
+let T; //? air temperature
+let Pr; //? atmospheric pressure
+let CD; //? drag coefficient
+
+let w; //? instantaneous angular velocity
+
+let CL = a1 * (K4 * Math.pow(Vf, 2) + K5);
+let rho = Pr / (R * T);
+
 
 export {
     g,
@@ -46,5 +52,6 @@ export {
     I,
     T,
     Pr,
-    CD
+    CD,
+    w
 };
